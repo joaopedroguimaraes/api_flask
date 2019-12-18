af914c3061195fb02624b3f741266b1c3369bb52
João Pedro Guimarães <joaopedroguimaraes96@gmail.com>
2019-12-18 16:53:55 -0300

    Generic request for get and post with validation

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET'])
-def receive_request():
+def receive_get_request():
     print("GET request")
     return "GET response"
 
@@ -13,6 +13,16 @@ def receive_request():
 def receive_post_request():
     print("POST request")
     return "POST response"
+
+
+@app.route("/1", methods=['GET', 'POST'])
+def receive_generic_request():
+    if request.method == 'GET':
+        print("GET response on /1")
+        return "GET response on /1"
+    else:
+        print("POST response on /1")
+        return "POST response on /1"
 
 
 if __name__ == "__main__":
--- requesting.py
+++ requesting.py
@@ -8,3 +8,13 @@ time.sleep(5)
 
 print("Enviando POST request")
 requests.post('http://127.0.0.1:5000/')
+
+time.sleep(5)
+
+print("Enviando GET request para /1")
+requests.get('http://127.0.0.1:5000/1')
+
+time.sleep(5)
+
+print("Enviando POST request para /1")
+requests.post('http://127.0.0.1:5000/1')
